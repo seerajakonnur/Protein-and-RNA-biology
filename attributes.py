9cf9a47cc6a368c11ea9b9c555a3a9952a892774
seerajakonnur <56847996+seerajakonnur@users.noreply.github.com>
2020-02-22 21:14:14 +0530

    Add files via upload

---
+++ attributes.py
@@ -0,0 +1,84 @@
+import re
+import array
+import sys
+import os
+import operator
+import pandas as pd
+import numpy as np
+from pandas import read_csv
+import csv
+import statistics
+import modlamp
+from modlamp.descriptors import PeptideDescriptor, GlobalDescriptor
+data=pd.read_csv("nef_pred_motifs.csv")
+arr_motifs=data.values
+#arr_motifs=arr[:,4]
+#print(arr_motifs)
+arr_len=[]
+motif=[]
+ch=[]
+ch_den=[]
+ip=[]
+ii=[]
+bi=[]
+hr=[]
+ar=[]
+al=[]
+for i in arr_motifs:
+ print(i[0])
+ motif.append(i[0])
+ arr_len.append(len(i[0]))
+
+
+# charge
+for i,j in zip(arr_motifs, arr_len):
+ desc = GlobalDescriptor(i[0])
+ desc.calculate_charge(ph=7.4, amide=True)
+ ch.append(desc.descriptor/j)
+
+
+#hydrophobic ratio
+for i,j in zip(arr_motifs, arr_len):
+ desc = GlobalDescriptor(i[0])
+ desc.hydrophobic_ratio()
+ hr.append(desc.descriptor/j)
+
+# aromaticity
+for i,j in zip(arr_motifs, arr_len):
+ desc = GlobalDescriptor(i[0])
+ desc.aromaticity()
+ ar.append(desc.descriptor/j)
+
+# aliphatic index
+for i,j in zip(arr_motifs, arr_len):
+ desc = GlobalDescriptor(i[0])
+ desc.aliphatic_index()
+ al.append(desc.descriptor/j)
+# ii
+
+data2=pd.read_csv("ii_nef_pred.csv")
+arr_ii=data2.values
+arr_ii2=arr_ii[:,0]
+print(arr_ii2)
+
+for i,j in zip(arr_ii2, arr_len):
+ ii.append(i/j)
+
+data2=pd.read_csv("ip_nef_pred.csv")
+arr_ip=data2.values
+arr_ip2=arr_ip[:,0]
+print(arr_ip2)
+
+for i,j in zip(arr_ip2, arr_len):
+ ip.append(i/j)
+
+df = pd.DataFrame({"motif" : motif, "charge" : ch, "hr" : hr,  "aromaticity" : ar, "aliphatic_index" : al, "ii" : ii, "ip" : ip })
+df.to_csv("attributes_nef_pred.csv", index=False)
+
+
+
+
+ 
+
+
+
